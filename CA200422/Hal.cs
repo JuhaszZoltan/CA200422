bbced25fa4945a22c8875f6bae69402f767e5900
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-04-22 19:43:53 +0200

    Add project files.

---
+++ CA200422/Hal.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200422
+{
+    enum Alfaj
+    {
+        Harcsa, Keszeg, Busa, Ponty,
+        Kecsege, Angolna, Amur, Polip,
+    }
+
+    public struct Eletter
+    {
+        public int MinMelyseg { get; set; }
+        public int MaxMelyseg { get; set; }
+
+        public Eletter(int minMelyseg, int maxMelyseg)
+        {
+            if (minMelyseg == maxMelyseg) maxMelyseg++;
+
+            if (minMelyseg > maxMelyseg)
+            {
+                MinMelyseg = maxMelyseg;
+                MaxMelyseg = minMelyseg;
+            }
+            else
+            {
+                MinMelyseg = minMelyseg;
+                MaxMelyseg = maxMelyseg;
+            }
+        }
+    }
+
+    class Hal
+    {
+        #region suly
+        private float _suly = -1;
+        public float Suly
+        {
+            set 
+            {
+                if (_suly == -1)
+                {
+                    if (value < 0.5F || value > 40F)
+                        throw new Exception("nem megfelelő érték a hal kezdősúlyának!");
+                    _suly = value;
+                }
+                else
+                {
+                    if (_suly * 1.1 < value || _suly * 0.9 > value)
+                        throw new Exception("ennyivel nem változhat a súlya egy lépésben!");
+                    _suly = value;
+                }
+            }
+            get
+            {
+                if (_suly == -1)
+                    throw new Exception("ez az érték még nincs beállítva");
+                return _suly;
+            }
+        }
+        #endregion
+        #region ragadozo
+        private bool _ragadozo;
+        private bool _ragadozoBeallitva = false;
+        public bool Ragadozo
+        {
+            set
+            {
+                if (_ragadozoBeallitva)
+                    throw new Exception("a halak nem változtatnak étrendet!");
+                _ragadozoBeallitva = true;
+                _ragadozo = value;
+            }
+            get
+            {
+                if (!_ragadozoBeallitva) throw new Exception("még nincs beállítva");
+                return _ragadozo;
+            }
+        }
+        #endregion
+        #region eletter
+        private Eletter _eletter;
+        private bool _eletterBeallitva = false;
+        public Eletter Eletter
+        {
+            set
+            {
+                if (_eletterBeallitva)
+                    throw new Exception("nem változtathat a hal életteret!");
+                if (value.MinMelyseg < 0)
+                    throw new Exception("nincsenek repülő halak");
+                if (value.MinMelyseg > 400 || value.MaxMelyseg > 400)
+                    throw new Exception("ilyen mélyen nem élhet a hal");
+                if (value.MaxMelyseg < 10)
+                    throw new Exception("ennél mélyebbre kell úsznia");
+                if (value.MaxMelyseg <= value.MinMelyseg)
+                    throw new Exception("a maximum nem lehet kevesebb vagy egyenlő, mint a minimum!");
+                _eletterBeallitva = true;
+                _eletter = value;
+            }
+            get
+            {
+                if (!_eletterBeallitva)
+                    throw new Exception("még nem volt rendesen beáálítva!");
+                return _eletter;
+            }
+        }
+        #endregion
+        #region alfaj
+        private Alfaj _alfaj;
+        private bool _alfajBellaitva = false;
+        public Alfaj Alfaj
+        {
+            set
+            {
+                if (_alfajBellaitva)
+                    throw new Exception("nem változhat meg a faja!");
+                _alfajBellaitva = true;
+                _alfaj = value;
+            }
+            get
+            {
+                if (!_alfajBellaitva) throw new Exception("még nem volt beállítva!");
+                return _alfaj;
+            }
+        }
+        #endregion
+    }
+}
---
+++ CA200422/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200422
+{
+    enum Het
+    {
+        Hetfo = 1,
+        Kedd = 2,
+        Szerda = 3,
+        Csutortok = 4,
+        Pentek = 5,
+        Szombat = 6,
+        Vasarnap = 7,
+    }
+
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main()
+        {
+            #region enumeration
+            ////init
+            //var nap = Het.Pentek;
+            ////rnd
+            //for (int i = 0; i < 100; i++) Console.Write((Het)rnd.Next(1, 8) + " ");
+            #endregion
+            #region halak
+
+            var halak = new List<Hal>();
+
+            int cRagadozo = 0;
+            int maxSulyI = 0;
+            int cEgyEgeszEgy = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                halak.Add(new Hal()
+                {
+                    Suly = rnd.Next(10, 800) / 20F,
+                    Ragadozo = rnd.Next(100) < 10,
+                    Alfaj = (Alfaj)rnd.Next(8),
+                    Eletter = new Eletter(rnd.Next(400), rnd.Next(10, 400)),
+                });
+
+                if (halak[i].Ragadozo) cRagadozo++;
+
+                if (halak[maxSulyI].Suly < halak[i].Suly) maxSulyI = i;
+
+                if (halak[i].Eletter.MinMelyseg <= 110 && halak[i].Eletter.MaxMelyseg >= 110)
+                    cEgyEgeszEgy++;
+            }
+
+            halak.Add(new Hal()
+            {
+                Alfaj = 0,
+                Ragadozo = false,
+                Suly = 35,
+                Eletter = new Eletter(350, 400),
+            });
+
+            Console.WriteLine($"Ragadozók száma:  {cRagadozo} db\nNövényevők száma: {halak.Count - cRagadozo} db");
+            Console.WriteLine($"Legnagyobb hal súlya: {halak[maxSulyI].Suly} Kg");
+            Console.WriteLine($"1.1 méteren élő halak száma: {cEgyEgeszEgy} db");
+
+
+            int dbNE = 0;
+            float kgNE = 0;
+
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var h1 = halak[rnd.Next(halak.Count)];
+                var h2 = halak[rnd.Next(halak.Count)];
+
+                if (h1.Ragadozo != h2.Ragadozo 
+                    && h1.Eletter.MaxMelyseg > h2.Eletter.MinMelyseg
+                    && h2.Eletter.MaxMelyseg > h1.Eletter.MinMelyseg
+                    && rnd.Next(100) < 30)
+                {
+                    if (!h1.Ragadozo)
+                    {
+                        kgNE += h1.Suly;
+                        halak.Remove(h1);
+                        h2.Suly *= rnd.Next(100, 110) / 100F;
+                    }
+                    else
+                    {
+                        kgNE += h2.Suly;
+                        halak.Remove(h2);
+                        h1.Suly *= rnd.Next(100, 110) / 100F;
+                    }
+                    dbNE++;
+                }
+            }
+
+            Console.WriteLine("\n\n------------------\n\n");
+
+            Console.WriteLine($"Megevett halak száma: {dbNE} db");
+            Console.WriteLine($"Összes megevett hal súlya: {kgNE} Kg");
+
+            Console.WriteLine("\n\n------------------\n\n");
+
+            foreach (var h in halak)
+            {
+                Console.WriteLine(
+                    "{0,-10} {1} {2: 00.00} Kg {3}",
+                    h.Alfaj,
+                    h.Ragadozo ? "R" : "N",
+                    h.Suly,
+                    $"[{h.Eletter.MinMelyseg};{h.Eletter.MaxMelyseg}]");
+            }
+
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
